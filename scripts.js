f95644e1f980f727f30c8140cc6eefcce42f136d
Vishwank-Chinnam <159262740+Vishwank-Chinnam@users.noreply.github.com>
2025-09-20 01:21:54 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
+<script>
+  const footerYear = document.querySelector("footer .footer-main p");
+  const currentYear = new Date().getFullYear();
+  footerYear.textContent = `© Vishwank Chinnam 2010–${currentYear}`;
+</script>
 
